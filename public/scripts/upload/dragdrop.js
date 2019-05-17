6904cdd17c554f6734df5f7d77a2c37a0932efe7
Maxim Snoep <m.snoep@student.tue.nl>
2019-05-17 12:58:02 +0200

    Drag and Drop file upload. Mimetypes fix.

---
+++ public/scripts/upload/dragdrop.js
@@ -0,0 +1,39 @@
+//// Drag Drop File Upload Function ////
+
+// Initialize Drag and Drop
+let dropArea = document.getElementById('drop-area')
+
+;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, preventDefaults, false)
+})
+
+function preventDefaults (e) {
+  e.preventDefault()
+  e.stopPropagation()
+}
+
+// Drag Handler
+;['dragenter', 'dragover'].forEach(eventName => {
+  dropArea.addEventListener(eventName, highlight, false)
+})
+
+;['dragleave', 'drop'].forEach(eventName => {
+  dropArea.addEventListener(eventName, unhighlight, false)
+})
+
+function highlight(e) { dropArea.classList.add('highlight') }
+function unhighlight(e) { dropArea.classList.remove('highlight') }
+
+
+// Drop Handler
+dropArea.addEventListener('drop', handleDrop, false)
+
+function handleDrop(e) {
+  let dt = e.dataTransfer
+  let files = dt.files
+  handleFiles(files)
+}
+
+function handleFiles(files) {
+  ([...files]).forEach(upload)
+}
--- public/scripts/upload/upload.js
+++ public/scripts/upload/upload.js
@@ -1,12 +1,11 @@
-//// UPLOAD SCRIPT ////
-
-// Select your INPUT_FILE and store it in a variable
-const input = document.getElementById('input');
+//// Global Upload Function ////
 
 // This will upload the file after having read it
 const upload = (file) => {
   var formData = new FormData();
   formData.append('file', file);
+
+  document.getElementById('file').innerHTML=`Uploaded file: ${file.name}`;
 
   console.log(file);
   console.log(formData);
@@ -19,9 +18,3 @@ const upload = (file) => {
   .catch(error => console.error('Error:', error))
   .then(response => console.log('Success:', JSON.stringify(response)))
 };
-
-// Event handler executed when a file is selected
-const onSelectFile = () =>{ upload(input.files[0]) };
-
-// Event listener to execute onSelectFile when a file has been selected
-input.addEventListener('change', onSelectFile, false);
