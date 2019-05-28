54797a9ac4e855a75fadbf63e32f210208d77c80
Konstantin <konstantinmvelkov@gmail.com>
2019-05-28 20:17:45 +0200

    Reordering interface for matrix interactions, rework of program flow of matrix.js

--- public/scripts/matrix/matrix.js
+++ public/scripts/matrix/matrix.js
@@ -1,7 +1,15 @@
 //// MATRIX SCRIPT ////
 
 // Function to initialize the matrix (Gets called from main file)
-loadMatrix = (box) => {
+var xValues = [];
+var yValues  = [];
+var zValues;
+var box;
+var colorscaleValue = [
+        [0, '#D3DFFF'],
+        [1, '#003DDE']
+    ];
+loadMatrix = (box1) => {
 
   // Initialize
   var filename = localStorage.getItem('selected_file');
@@ -14,12 +22,13 @@ loadMatrix = (box) => {
   })
   .then(function(myJson) {
     file = JSON.parse(JSON.stringify(myJson));
+    box = box1;
     // Run Visualise with file
     Visualise(file, box);
   });
 
 }
-function AlphabeticalOrder(xValues,yValues,zValues) {
+function AlphabeticalOrder(xVal,yVal,zVal) {
     function matrix(rows, cols, defaultValue){
         var arr = [];
         // Creates all lines:
@@ -36,26 +45,26 @@ function AlphabeticalOrder(xValues,yValues,zValues) {
         return arr;
     }
     var sortAr = new Array();
-    for(var i =0; i < xValues.length; i++) {
-        sortAr.push(xValues[i]);
+    for(var i =0; i < xVal.length; i++) {
+        sortAr.push(xVal[i]);
     }
     sortAr = sortAr.sort();
     var permutation = new Array();
-    for(var i = 0; i < xValues.length; i++) {
-        for(var j = 0; j < xValues.length; j++) {
+    for(var i = 0; i < xVal.length; i++) {
+        for(var j = 0; j < xVal.length; j++) {
             //.valueOf()
-            if(xValues[i]==sortAr[j]) {
+            if(xVal[i]==sortAr[j]) {
                 permutation.push(j);
                 break;
             }
         }
     }
 
-    var alphaZValues = matrix(zValues[0].length,zValues[0].length,"*")
-    var reorderedZValues = matrix(zValues[0].length,zValues[0].length,"*")
-    for(var i = 0; i < zValues[0].length; i++) {
-      for(var j = 0; j < zValues[0].length; j++) {
-          alphaZValues[i][j] = zValues[i][permutation[j]];
+    var alphaZValues = matrix(zVal[0].length,zVal[0].length,"*")
+    var reorderedZValues = matrix(zVal[0].length,zVal[0].length,"*")
+    for(var i = 0; i < zVal[0].length; i++) {
+      for(var j = 0; j < zVal[0].length; j++) {
+          alphaZValues[i][j] = zVal[i][permutation[j]];
       }
     }
     for(var i = 0; i < zValues[0].length; i++) {
@@ -63,10 +72,12 @@ function AlphabeticalOrder(xValues,yValues,zValues) {
           reorderedZValues[j][i] = alphaZValues[permutation[j]][i];
       }
     }
-    var returnArray = [sortAr,sortAr,reorderedZValues];
-    return returnArray;
+    DisplayGraph(sortAr,sortAr,reorderedZValues)
+    //var returnArray = [sortAr,sortAr,reorderedZValues];
+    //return returnArray;
+    return;
 }
-function SumOrder(xValues,yValues,matrix) {
+function SumOrder(xVal,yVal,matrix) {
     var rows = new Array(matrix[0].length).fill(0);
     for(var i = 0; i < matrix[0].length; i++){
       for(var j = 0; j < matrix[0].length; j++){
@@ -126,16 +137,24 @@ function SumOrder(xValues,yValues,matrix) {
         order_matrix[i][j] = matrix[rows_index[i]][cols_index[j]];
       }
     }
-    var reorderedXValues = new Array(xValues.length).fill(0);
-    var reorderedYValues = new Array(yValues.length).fill(0);
-    for(var i = 0; i < xValues.length;i++) {
-        reorderedXValues[i] = xValues[cols_index[i]];
-        reorderedYValues[i] = yValues[rows_index[i]];
+    var reorderedXValues = new Array(xVal.length).fill(0);
+    var reorderedYValues = new Array(yVal.length).fill(0);
+    for(var i = 0; i < xVal.length;i++) {
+        reorderedXValues[i] = xVal[cols_index[i]];
+        reorderedYValues[i] = yVal[rows_index[i]];
     }
-    var returnArray = [reorderedXValues,reorderedYValues,order_matrix]
-    return returnArray;
+    
+    /*colorscaleValue = [
+        [0, '#D3DFFF'],
+        [1, '#003DDE']
+    ];*/
+    
+    DisplayGraph(reorderedXValues,reorderedYValues,order_matrix)
+    //var returnArray = [reorderedXValues,reorderedYValues,order_matrix]
+    //return returnArray;
+    return;
 }
-function AverageOrder(xValues,yValues,initial_matrix) {
+function AverageOrder(xVal,yVal,initial_matrix) {
 
     var rows = new Array(initial_matrix[0].length).fill(1);
     for(var i = 0; i < initial_matrix[0].length; i++){
@@ -206,32 +225,33 @@ function AverageOrder(xValues,yValues,initial_matrix) {
       }
     }
 
-    var reorderedXValues = new Array(xValues.length).fill(0);
-    var reorderedYValues = new Array(yValues.length).fill(0);
-    for(var i = 0; i < xValues.length;i++) {
-        reorderedXValues[i] = xValues[cols_index[i]];
-        reorderedYValues[i] = yValues[rows_index[i]];
+    var reorderedXValues = new Array(xVal.length).fill(0);
+    var reorderedYValues = new Array(yVal.length).fill(0);
+    for(var i = 0; i < xVal.length;i++) {
+        reorderedXValues[i] = xVal[cols_index[i]];
+        reorderedYValues[i] = yVal[rows_index[i]];
     }
-
-    var returnArray = [reorderedXValues,reorderedYValues,order_matrix]
-    return returnArray;
+    
+    DisplayGraph(reorderedXValues,reorderedYValues,order_matrix)
+    //var returnArray = [reorderedXValues,reorderedYValues,order_matrix]
+    //return returnArray;
+    return;
 }
-function UpdateGraphColor(xValues,yValues,zValues,box) {
+function UpdateGraphColor(xVal,yVal,zVal,box) {
     let attributes = [];
     attributes[0] = document.getElementById('startColor').value;
     attributes[1] = document.getElementById('endColor').value;
-    console.log(attributes[0]);
-    console.log(attributes[1]);
-     var colorscaleValue = [
+    colorscaleValue = [
         [0, attributes[0].toString()],
         [1, attributes[1].toString()]
     ];
   //Input data for heatmap
-    var data = [
+    DisplayGraph(xVal,yVal,zVal)
+    /*var data = [
     {
         z: zValues,
-        x: xValues,
-        y: yValues,
+        x: xVal,
+        y: yVal,
         colorscale: colorscaleValue,
         type: 'heatmap'
     }
@@ -242,15 +262,50 @@ function UpdateGraphColor(xValues,yValues,zValues,box) {
   
 
   // Run Heatmap
-  Plotly.newPlot(`${box}`, data, layout,{showSendToCloud: true});
+  Plotly.newPlot(`${box}`, data, layout,{showSendToCloud: true});*/
     
+}
+function SelectReordering(selectTag) {
+            var selIndexes = "";
+            console.log(zValues);
+            for (var i = 0; i < selectTag.options.length; i++) {
+                var optionTag = selectTag.options[i];
+                if (optionTag.selected) {
+                    if (selIndexes.length > 0)
+                        selIndexes += ", ";
+                    selIndexes += optionTag.text;
+                }
+            }
+
+            var info = document.getElementById ("info");
+            if (selIndexes.length > 0) {
+                console.log("Selected options: " + selIndexes);
+                if(selIndexes=="Original") {
+                    console.log("Original");
+                    loadMatrix(box);
+                }
+                if(selIndexes=="Alphabetical") {
+                    console.log("Alphabetical");
+                    AlphabeticalOrder(xValues,yValues,zValues);
+                }
+                if(selIndexes=="Sum") {
+                    console.log("Sum");
+                    SumOrder(xValues,yValues,zValues);
+                }
+                if(selIndexes=="Average") {
+                    console.log("Average");
+                    AverageOrder(xValues,yValues,zValues);
+                }   
+            }
+            else {
+                console.log("There is no selected option");
+            }
 }
 // Main Visualise Function
 function Visualise(file, box) {
 
   // Initialize
-  var xValues = [];
-  var yValues  = [];
+  
 
   // Build the name values
   for(var i = 1; i < file.length; i++) {
@@ -301,7 +356,7 @@ function Visualise(file, box) {
     }
   }
   // Build a Matrix for zValues
-  var zValues = matrix(file[0].length-1,file[0].length-1,"*");
+  zValues = matrix(file[0].length-1,file[0].length-1,"*");
 
   // Build a Table for zValues
   var table = matrix(file[0].length,file[0].length-1,"*");
@@ -317,30 +372,23 @@ function Visualise(file, box) {
       zValues[i][j]=array[i+1][j+1];
     }
   }
-    //some reordering from reorder.js
-    //console.log(zValues);
-    //var perm = reorder.optimal_leaf_order()(zValues);
-    //console.log(perm);
-    //var permuted_mat = reorder.stablepermute(zValues, perm);
-    //console.log(permuted_mat);
-    //console.log(permuted_mat);
 
-  //color diapason of the tiles
-    var colorscaleValue = [
-        [0, '#D3DFFF'],
-        [1, '#003DDE']
-    ];
-    var returnAlphaBeticalOrder = AlphabeticalOrder(xValues,yValues,zValues);
-    var returnSumOrder = SumOrder(xValues,yValues,zValues);
-    var returnAverageOrder = AverageOrder(xValues,yValues,zValues);
+    //var returnAlphaBeticalOrder = AlphabeticalOrder(xValues,yValues,zValues);
+    //var returnSumOrder = SumOrder(xValues,yValues,zValues);
+    //var returnAverageOrder = AverageOrder(xValues,yValues,zValues);
     console.log(zValues);
     //console.log(returnSumOrder[2]);
   //Input data for heatmap
- var data = [
+ DisplayGraph(xValues,yValues,zValues);
+document.getElementById("updateHeatmap").addEventListener("click",function() {UpdateGraphColor(xValues,yValues,zValues,box)} );
+    
+}
+function DisplayGraph(xVal,yVal,zVal) {
+    var data = [
     {
-    z: zValues,
-    x: xValues,
-    y: yValues,
+    z: zVal,
+    x: xVal,
+    y: yVal,
     colorscale: colorscaleValue,
     type: 'heatmap'
     }
@@ -371,6 +419,4 @@ var layout = {
 
   // Run Heatmap
   Plotly.newPlot(`${box}`, data, layout,{showSendToCloud: true});
-document.getElementById("updateColorHeatmap").addEventListener("click",function() {UpdateGraphColor(xValues,yValues,zValues,box)} );
-    /*updateColorHeatmapBtn.addEventListener("click", UpdateGraphColor(xValues,yValues,zValues,box),false);*/
 }
